96e2d3ddad5ed6fd8b2993822cfbc761fc3d4a23
Vitor Marinho <vitormarinho@gufufu.co.uk>
2019-09-03 12:25:34 +0100

    new pages added

---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>This is the Contact Page</h1>
+            <p>This page will be updated with my contact details for future reference.</p>
+        </div>
+    )
+
+}
+
+export default ContactPage
---
+++ src/pages/work.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+const WorkPage = () => {
+    return (
+        <div>
+            <h1>This is the Portfolio Page</h1>
+            <p>Here on this page will be the portfolio list showcase.</p>
+        </div>
+    )
+
+}
+
+export default WorkPage
